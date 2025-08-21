8b95344a99d9320717a18aec417f124a87f54fe4
Igor Nunes Patricio <igornunespatricio@gmail.com>
2025-08-21 09:26:01 -0300

    adding logs to scraper

--- src/scraper.py
+++ src/scraper.py
@@ -33,40 +33,48 @@ class SerieAScraper:
 
     def scrape_basic_match_data(self):
         """Scrape and save to database"""
-        soup = self._get_page(self.url)
-        rows = soup.select(
-            "table.stats_table tbody tr[data-row]:not(.spacer.partial_table.result_all, .thead)"
-        )
-        logger.info(f"Found {len(rows)} matches to scrape")
-        for row in rows:
+        try:
 
-            if match := self._extract_match_data(row):
-                print(match)
-                self.db.execute_query(
-                    f"INSERT OR IGNORE INTO {RAW_TABLE} (date, home, score, away, attendance, report_link) "
-                    "VALUES (?, ?, ?, ?, ?, ?)",
-                    (
-                        match["date"],
-                        match["home"],
-                        match["score"],
-                        match["away"],
-                        match["attendance"],
-                        match["report_link"],
-                    ),
-                )
-                logger.info(
-                    f"Saved {match['home']} {match['score']} {match['away']} to database - {match['report_link']}"
-                )
+            soup = self._get_page(self.url)
+            rows = soup.select(
+                "table.stats_table tbody tr[data-row]:not(.spacer.partial_table.result_all, .thead)"
+            )
+            logger.info(f"Found {len(rows)} matches to scrape")
+            for row in rows:
+
+                if match := self._extract_match_data(row):
+                    print(match)
+                    self.db.execute_query(
+                        f"INSERT OR IGNORE INTO {RAW_TABLE} (date, home, score, away, attendance, report_link) "
+                        "VALUES (?, ?, ?, ?, ?, ?)",
+                        (
+                            match["date"],
+                            match["home"],
+                            match["score"],
+                            match["away"],
+                            match["attendance"],
+                            match["report_link"],
+                        ),
+                    )
+                    logger.info(
+                        f"Saved {match['home']} {match['score']} {match['away']} to database - {match['report_link']}"
+                    )
+        except Exception as e:
+            logger.error(f"Error while scraping basic match data: {e}")
 
     def _get_page(self, url: str) -> BeautifulSoup:
         """Load page with configured delay"""
-        self.driver.get(url)
-        WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "table.stats_table"))
-        )
-        logger.debug(f"Waiting {REQUEST_DELAY:.1f}s after request")
-        time.sleep(REQUEST_DELAY)
-        return BeautifulSoup(self.driver.page_source, "html.parser")
+        try:
+            self.driver.get(url)
+            WebDriverWait(self.driver, 15).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "table.stats_table"))
+            )
+            logger.debug(f"Waiting {REQUEST_DELAY:.1f}s after request")
+            time.sleep(REQUEST_DELAY)
+            return BeautifulSoup(self.driver.page_source, "html.parser")
+        except Exception as e:
+            logger.error(f"Error loading page: {e}")
+        return None
 
     def _extract_match_data(self, row) -> Optional[Dict]:
         """Extract basic match info from schedule row"""
@@ -76,66 +84,76 @@ class SerieAScraper:
             text = text.strip()
             return text if text != "" else None
 
-        date = clean_text(row.select_one("td[data-stat='date']").text)
-        home = clean_text(row.select_one("td[data-stat='home_team']").text)
-        score = clean_text(row.select_one("td[data-stat='score']").text)
-        away = clean_text(row.select_one("td[data-stat='away_team']").text)
-        attendance = clean_text(row.select_one("td[data-stat='attendance']").text)
-        report_link = row.select_one('td[data-stat="match_report"] a')
-        if report_link and "/en/matches/" in report_link["href"]:
-            report_link = f'https://fbref.com{report_link["href"]}'
-        else:
-            report_link = None
-        results = {
-            "date": date,
-            "home": home,
-            "score": score,
-            "away": away,
-            "attendance": attendance,
-            "report_link": report_link,
-        }
-        return results
+        try:
+            date = clean_text(row.select_one("td[data-stat='date']").text)
+            home = clean_text(row.select_one("td[data-stat='home_team']").text)
+            score = clean_text(row.select_one("td[data-stat='score']").text)
+            away = clean_text(row.select_one("td[data-stat='away_team']").text)
+            attendance = clean_text(row.select_one("td[data-stat='attendance']").text)
+            report_link = row.select_one('td[data-stat="match_report"] a')
+            if report_link and "/en/matches/" in report_link["href"]:
+                report_link = f'https://fbref.com{report_link["href"]}'
+            else:
+                report_link = None
+            results = {
+                "date": date,
+                "home": home,
+                "score": score,
+                "away": away,
+                "attendance": attendance,
+                "report_link": report_link,
+            }
+            return results
+        except Exception as e:
+            logger.error(f"Error extracting match data: {e}")
+        return None
 
     # TODO: create method for team stats
     def _extract_team_stats(self, soup: BeautifulSoup):
         """Extract team_stats from report link"""
-        team_stats_div = soup.select_one("div#team_stats")
-        if team_stats_div:
-            return str(team_stats_div)
+        try:
+            team_stats_div = soup.select_one("div#team_stats")
+            if team_stats_div:
+                return str(team_stats_div)
+        except Exception as e:
+            logger.error(f"Error extracting team stats: {e}")
         return None
 
     # TODO: create method for extra stats
     def _extract_extra_stats(self, soup: BeautifulSoup):
         """Extract extra_stats from report link"""
-        team_extra_stats_div = soup.select_one("div#team_stats_extra")
-        if team_extra_stats_div:
-            return str(team_extra_stats_div)
+        try:
+            team_extra_stats_div = soup.select_one("div#team_stats_extra")
+            if team_extra_stats_div:
+                return str(team_extra_stats_div)
+        except Exception as e:
+            logger.error(f"Error extracting extra stats: {e}")
         return None
 
-    def scrape_match_reports(self):
+    def scrape_match_reports(self, year: Optional[int] = None):
         """Scrape match reports and save to database"""
-        # query the database to get report links with no team_stats or extra_stats
-        matches = self.db.execute_query(
-            f"SELECT * FROM {RAW_TABLE} WHERE report_link IS NOT NULL AND team_stats IS NULL AND extra_stats IS NULL"
-        )
-        count = 0
-        for match in matches:
-            soup = self._get_page(match["report_link"])
-            team_stats = self._extract_team_stats(soup)
-            extra_stats = self._extract_extra_stats(soup)
+        try:
+            query = f"SELECT * FROM {RAW_TABLE} WHERE report_link IS NOT NULL AND team_stats IS NULL AND extra_stats IS NULL"
+            if year:
+                query += f" AND date LIKE '{year}%'"
+            matches = self.db.execute_query(query)
+            logger.info(f"Found {len(matches)} matches to scrape")
+            for match in matches:
+                soup = self._get_page(match["report_link"])
+                team_stats = self._extract_team_stats(soup)
+                extra_stats = self._extract_extra_stats(soup)
 
-            self.db.execute_query(
-                f"UPDATE {RAW_TABLE} SET team_stats = ?, extra_stats = ? WHERE report_link = ?",
-                (team_stats, extra_stats, match["report_link"]),
-            )
+                self.db.execute_query(
+                    f"UPDATE {RAW_TABLE} SET team_stats = ?, extra_stats = ? WHERE report_link = ?",
+                    (team_stats, extra_stats, match["report_link"]),
+                )
 
-            logger.info(
-                f"Saved team stats and extra stats for {match['home']} {match['score']} {match['away']} to database - {match['report_link']}"
-            )
-            count += 1
+                logger.info(
+                    f"Saved team stats and extra stats for {match['home']} {match['score']} {match['away']} to database - {match['report_link']}"
+                )
 
-            if count >= 1:
-                break
+        except Exception as e:
+            logger.error(f"Error while scraping match reports: {e}")
 
 
 if __name__ == "__main__":
@@ -145,7 +163,7 @@ if __name__ == "__main__":
     try:
         scraper = SerieAScraper(driver, url=URLS[0])
         # scraper.scrape_basic_match_data()
-        scraper.scrape_match_reports()
+        scraper.scrape_match_reports(year=2025)
 
     finally:
         driver_manager.close()
